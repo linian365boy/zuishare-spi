80649a8f9f3516192e86f33980b028d8c85daabe
linian365boy <linian365boy@foxmail.com>
2018-07-21 12:41:36 +0800

    feat(zuishare-spi): 完善spi

--- src/main/java/top/zuishare/spi/model/Advertisement.java
+++ src/main/java/top/zuishare/spi/model/Advertisement.java
@@ -1,5 +1,6 @@
 package top.zuishare.spi.model;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -115,6 +116,6 @@ public class Advertisement implements Serializable {
 
 	@Override
 	public String toString() {
-		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+		return ReflectionToStringBuilder.toStringExclude(this, comment);
 	}
 }
